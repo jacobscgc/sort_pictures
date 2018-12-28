7b83291824ab0b13bdae40f84f42181ff6c09e27
jacobscgc <39371572+jacobscgc@users.noreply.github.com>
2018-12-28 15:43:42 +0100

    Update sort_pictures.py
    1) Modified script to only create folders for which pictures were found.
    2) Added a bit of code so it can handle pictures without exif data (which are send to unsorted folder).
    3) Added message box which states the number of sorted pictures.

--- sort_pictures.py
+++ sort_pictures.py
@@ -7,115 +7,105 @@ the correct folder.
 This script depends on exifread (pip install exifread).
 
 Run the script with:
-	python sort_pictures.py
-	
+    python sort_pictures.py
+    
 Output folder cannot exist. Choose a different output folder name or delete the existing output folder first.
 """
 
 import tkinter as tk
-from tkinter import filedialog, simpledialog
+from tkinter import filedialog, messagebox
 import exifread
 import os
-from shutil import copyfile
+from shutil import copyfile, copy
 
 root = tk.Tk()
 root.withdraw()
 
 mypath = filedialog.askdirectory(title = "Please select the photo folder")
-output_folder_name = simpledialog.askstring("Input", "Please specify an output directory name.")
-output_folder = mypath + '/' + output_folder_name
+output_folder = mypath + '/sorted'
+unsorted_folder = output_folder + '/unsorted'
 
 # Check whether the image folder exists:
 if not os.path.exists(mypath):
-	print('Path {0} does not exist, check the location of your images and try again'.format(mypath))
-	exit()
+    print('Path {0} does not exist, check the location of your images and try again'.format(mypath))
+    exit()
 
 # If the output folder already exists, exit:
 if os.path.exists(output_folder):
-	print('Output folder {0} already exists, use a different output folder or delete the old one first.'.format(output_folder))
-	exit()
+    print('Output folder {0} already exists, use a different output folder or delete the old one first.'.format(output_folder))
+    exit()
 
 # Create the output folder:
 os.mkdir(output_folder)
-		
+# Create a rest folder for the images that cannot be sorted (no exif data)
+os.mkdir(unsorted_folder)
+        
 # put all filenames in a list:
 files = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
 
-def _create_directories(output_folder, year):
-	"""
-	Helper function to create the directory structure for the output.
-	
-	:param output_folder = the folder to write the output to.
-	"""
+def _create_directories(output_folder, year, year_month):
+    """
+    Helper function to create the directory structure for the output.
+    
+    :param output_folder = the folder to write the output to.
+    :param year = list of years present in the set of pictures.
+    :param year_month = list of tuples (year, month) present in the set of pictures.
+    """
 
-	# Create the directory structure:
-	january = output_folder + '/' + year + '/01_January'
-	february = output_folder + '/' + year + '/02_February'
-	march = output_folder + '/' + year + '/03_March'
-	april = output_folder + '/' + year + '/04_April'
-	may = output_folder + '/' + year + '/05_May'
-	june = output_folder + '/' + year + '/06_June'
-	july = output_folder + '/' + year + '/07_July'
-	august = output_folder + '/' + year + '/08_August'
-	september = output_folder + '/' + year + '/09_September'
-	october = output_folder + '/' + year + '/10_October'
-	november = output_folder + '/' + year + '/11_November'
-	december = output_folder + '/' + year + '/12_December'
-
-	os.mkdir(january)
-	os.mkdir(february)
-	os.mkdir(march)
-	os.mkdir(april)
-	os.mkdir(may)
-	os.mkdir(june)
-	os.mkdir(july)
-	os.mkdir(august)
-	os.mkdir(september)
-	os.mkdir(october)
-	os.mkdir(november)
-	os.mkdir(december)
+    month_list = ['01_January', '02_February', '03_March', '04_April', '05_May', '06_June', '07_July', '08_August', '09_September', '10_October', '11_November', '12_December']
+    for month in month_list:
+        if (year, month) in year_month:
+            os.mkdir(output_folder + '/' + year + '/' + month)
 
 def _convert_month(month):
-	"""
-	Helper function to translate the months from 01-12 to January - December.
-	
-	:param month: Month in numbers 01-12
-	return month in January - December.
-	"""
-	month_dict = {'01':'01_January', '02':'02_February', '03':'03_March', '04':'04_April', '05':'05_May', '06':'06_June', '07':'07_July', '08':'08_August', '09':'09_September', '10':'10_October', '11':'11_November', '12':'12_December'}
-	return month_dict[month]
+    """
+    Helper function to translate the months from 01-12 to January - December.
+    
+    :param month: Month in numbers 01-12
+    return month in January - December.
+    """
+    month_dict = {'01':'01_January', '02':'02_February', '03':'03_March', '04':'04_April', '05':'05_May', '06':'06_June', '07':'07_July', '08':'08_August', '09':'09_September', '10':'10_October', '11':'11_November', '12':'12_December'}
+    return month_dict[month]
 
 years = []
+year_month = []
 # First, check for which years pictures are present:
 for file_name in files:
-	# Open image file for reading (binary mode)
-	f = open(mypath + '/' + file_name, 'rb')
-	# Return Exif tags
-	tags = exifread.process_file(f)
-	date_taken = tags['EXIF DateTimeOriginal']
-	year = str(date_taken).split(':')[0]
-	if year not in years:
-		years.append(year)
+    # Open image file for reading (binary mode)
+    f = open(mypath + '/' + file_name, 'rb')
+    # Return Exif tags
+    tags = exifread.process_file(f)
+    if 'EXIF DateTimeOriginal' in tags:
+        date_taken = tags['EXIF DateTimeOriginal']
+        year = str(date_taken).split(':')[0]
+        month = _convert_month(str(date_taken).split(':')[1])
+        year_month_tup = (year, month)
+        if year not in years:
+            years.append(year)
+        if year_month_tup not in year_month:
+                    year_month.append(year_month_tup)
 
 # Create the directories for each year:
 for year in years:
-	os.mkdir(output_folder + '/' + year)
-	_create_directories(output_folder, year)
+    os.mkdir(output_folder + '/' + year)
+    _create_directories(output_folder, year, year_month)
 
 # Loop through the files, copy each file in the correct directory:
 counter = 0
 for file_name in files:
-	# Open image file for reading (binary mode)
-	fileloc = mypath + '/' + file_name
-	f = open(fileloc, 'rb')
-	# Return Exif tags
-	tags = exifread.process_file(f)
-	date_taken = tags['EXIF DateTimeOriginal']
-	year = str(date_taken).split(':')[0]
-	month = str(date_taken).split(':')[1]
-	month = _convert_month(month)
-	copy_loc = output_folder + '/' + year + '/' + month + '/' + file_name
-	copyfile(fileloc, copy_loc)
-	counter += 1
+    # Open image file for reading (binary mode)
+    fileloc = mypath + '/' + file_name
+    f = open(fileloc, 'rb')
+    # Return Exif tags
+    tags = exifread.process_file(f)
+    if 'EXIF DateTimeOriginal' in tags:
+        date_taken = tags['EXIF DateTimeOriginal']
+        year = str(date_taken).split(':')[0]
+        month = _convert_month(str(date_taken).split(':')[1])
+        copy_loc = output_folder + '/' + year + '/' + month + '/' + file_name
+        copyfile(fileloc, copy_loc)
+    else:
+        copy(fileloc, unsorted_folder)
+    counter += 1
 
-print('Done, sorted {0} pictures over a period of {1} years.'.format(counter, len(years)))
+messagebox.showinfo("", 'Done, sorted {0} pictures over a period of {1} years.'.format(counter, len(years)))
